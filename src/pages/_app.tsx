f4ee86a1279992e5658051c1549c93053a0c111f
Tomas Zmiejevas <tomas.zmiejevas@gmail.com>
2023-04-05 23:54:15 -0700

    Progress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider} from "@chakra-ui/react"
 import { theme } from '../chakra/theme'
+import Layout from '../components/Layout/Layout'
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ChakraProvider>
-    <Component {...pageProps} />
+  <ChakraProvider theme={theme}>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   </ChakraProvider>
   )
 }
+
+export default MyApp
