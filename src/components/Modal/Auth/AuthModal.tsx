660322cb15fdd9caddbf95d77bfcd6bdcc06ce78
Tomas Zmiejevas <tomas.zmiejevas@gmail.com>
2023-04-10 23:43:28 -0700

    Little progress

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,3 +1,4 @@
+import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from '@chakra-ui/react';
 import React from 'react';
 
 
@@ -6,6 +7,22 @@ const AuthModal:React.FC<AuthModalProps> = () => {
     
     return (
         <>
+        <Button onClick={onOpen}>Open Modal</Button>
+        <Modal>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Lorem count={2} />
+                </ModalBody>
+
+                <ModalFooter>
+                    <Button colorScheme="blue" mr={3} onClick={onClose}>Close</Button>
+                    <Button variant="ghost">Secondary Action</Button>
+                </ModalFooter>
+            </ModalContent>
+        </Modal>
         </>
     )
 }
