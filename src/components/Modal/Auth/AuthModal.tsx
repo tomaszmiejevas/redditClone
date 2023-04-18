abb20861bd2aef991bf5a889a9c9fe91311775df
Tomas Zmiejevas <tomas.zmiejevas@gmail.com>
2023-04-17 23:29:02 -0700

    Progress -> Might be tricky to avoid adding firebase api stuff...

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,8 +1,9 @@
 import { authModalState } from '@/atoms/authModalAtom';
-import { Button, Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react';
+import { Button, Flex, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, Text } from '@chakra-ui/react';
 import React from 'react';
 import { useRecoilState } from 'recoil';
 import AuthInputs from './AuthInputs';
+import OAuthButtons from './OAuthButtons';
 
 
 
@@ -29,9 +30,10 @@ const AuthModal:React.FC = () => {
                 <ModalCloseButton />
                 <ModalBody display='flex' flexDirection='column' alignItems='center' justifyContent='center' pb={6}>
                     <Flex direction='column' align='center' justify='center' width='70%'>
-                        {/* <OAuthButton />*/
+                        <OAuthButtons />
+                        <Text color="gray.500" fontWeight={700}>OR</Text>
                         <AuthInputs />
-                        /*<ResetPassword /> */}
+                        {/*<ResetPassword /> */}
                     </Flex>
                 </ModalBody>
             </ModalContent>
--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,13 +1,16 @@
-import { Button, Flex } from '@chakra-ui/react';
+import { Button, Flex, Image} from '@chakra-ui/react';
 import React from 'react';
 
 
 const OAuthButtons:React.FC = () => {
     
     return (
-        <Flex>
-            <Button>Continue with Google</Button>
-        </Flex>
-    )
+      <Flex direction="column" width="100%" mb={4}>
+        <Button variant="oauth" mb={2}>
+          <Image src="/images/googlelogo.png" height="20px" mr={4} />
+          Continue with Google
+        </Button>
+      </Flex>
+    );
 }
 export default OAuthButtons;
